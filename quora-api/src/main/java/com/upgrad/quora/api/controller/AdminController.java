ca7844713fb5ac5992ca1f2a05c25db5424a5aa7
h1m <himanshu.yadav643@gmail.com>
2021-08-04 02:36:48 +0530

    working on the AdminController definition

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AdminController.java
@@ -1,4 +1,21 @@
 package com.upgrad.quora.api.controller;
 
+import com.upgrad.quora.api.model.UserDeleteResponse;
+import com.upgrad.quora.service.business.UserBusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
 public class AdminController {
+
+    @Autowired
+    private UserBusinessService userBusinessService;
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/admin/user/{userId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<UserDeleteResponse> userDelete(@PathVariable("userId") final String userUuid, @RequestHeader("authorization") final String authorization) {
+
+    }
 }
